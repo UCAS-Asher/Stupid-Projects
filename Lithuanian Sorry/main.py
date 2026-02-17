e389f84f1e53195b8fdd20591e996a85206a6197
UCAS-Asher <asher.wangia@ucas-edu.net>
2026-02-17 19:44:49 +0000

    Working on it

--- Lithuanian Sorry/main.py
+++
@@ -1,1 +0,0 @@
-#Lithuianian Roulete
---
+++ Lithuianian Roulete/main.py
@@ -0,0 +1,7 @@
+#Lithuianian Roulete
+
+
+print("This a like Rushian Roulete, you must avoid the enemy shooting you with the shot.")
+
+def main():
+    print("Lithuianian Roulete\n 1. Play Game\n 2.Check Scores\n 3. Exit")
