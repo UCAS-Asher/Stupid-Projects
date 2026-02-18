64624e77918b84f15cb10775b800e4b050d9abf9
UCAS-Asher <asher.wangia@ucas-edu.net>
2026-02-18 21:35:56 +0000

    Working

--- Lithuianian Roulete/main.py
+++ Lithuianian Roulete/main.py
@@ -7,16 +7,47 @@ print("This a like Rushian Roulete, you must avoid the enemy shooting you with t
 
 def game_loop(lives, enemy_lives, gun):
     print(f"Your Turn\nLives:{lives}\nEnemy Lives:{enemy_lives}")
-    choice = input("Shoot(Y/N)")
+    choice = input("Shoot(Y/N): ")
 
-    if choice == "Y" or "y":
-        if gun[0] == 1:
+    if choice == "Y" or choice == "y":
+        if gun[0] == "S":
+            print("The gun clicked... A loud boom was heard... Ehh sorry...")
+            enemy_lives -= 1
+            if enemy_lives < 1:
+                print("You won... Ehh nepala...")
+                return
+            gun = random.sample(["B","B","B","B","B","S"], 6)
+        else:
+            gun.remove(gun[0])
+            print("The gun clicked... It was empty")
+    elif choice == "N" or choice == "n":
+        print("You forfeit the gun... You hope it doesnt have the shot")
+    else:
+        print("You didnt feel like trying... You gave up your turn")
+
+    comp_shoot = random.choice(["Y","N"])
+
+    if comp_shoot == "Y":
+        if gun[0] == "S":
+            print("The gun clicked... A loud laugh was heard... HEHEHEHA!\n")
+            lives -= 1
+            if lives < 1:
+                print("You lost... MEGAKNIGHT!")
+                return
+            gun = random.sample(["B","B","B","B","B","S"], 6)
+        else:
+            print("The gun clicked... He didnt BM...\n")
+    else:
+        print("He waited planning out his next move...\n")
+
+    game_loop(lives, enemy_lives, gun)
             
 
 def game():
     print("Loading the gun...\nYou have 3 lives, dont die\n")
-    gun = random.shuffle([0,0,0,0,0,1])
+    gun = random.sample(["B","B","B","B","B","S"], 6)
     game_loop(3,3,gun)
+    main()
 
 
 def scores():
