123fbaab455a844d47cb5c5b97a5963e7bd774da
UCAS-Asher <asher.wangia@ucas-edu.net>
2026-02-18 19:44:24 +0000

    working on it

--- Lithuianian Roulete/main.py
+++ Lithuianian Roulete/main.py
@@ -1,17 +1,30 @@
 #Lithuianian Roulete
 
+import random
+
 
 print("This a like Rushian Roulete, you must avoid the enemy shooting you with the shot.")
 
+def game_loop(lives, enemy_lives, gun):
+    print(f"Your Turn\nLives:{lives}\nEnemy Lives:{enemy_lives}")
+    choice = input("Shoot(Y/N)")
+
+    if choice == "Y" or "y":
+        if gun[0] == 1:
+            
+
 def game():
-    pass
+    print("Loading the gun...\nYou have 3 lives, dont die\n")
+    gun = random.shuffle([0,0,0,0,0,1])
+    game_loop(3,3,gun)
+
 
 def scores():
     pass
 
 
 def main():
-    print("Lithuianian Roulete\n 1. Play Game\n 2.Check Scores\n 3. Exit")
+    print("Lithuianian Roulete\n1. Play Game\n2.Check Scores\n3. Exit")
 
     choice = input("Choose a Number: ")
 
